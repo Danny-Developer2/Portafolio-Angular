c2399215eee87c8c8d0335ce82536a140ef991c6
Daniel Luevano <daniel_luevanolr@hotmail.com>
2024-11-29 01:11:44 -0600

    build 5

---
+++ Portafolio/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Servir los archivos estáticos de la build de Angular
+app.use(express.static(path.join(__dirname, 'dist/portafolio/browser')));
+
+// Ruta para cualquier otra solicitud, servir el index.html de Angular
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/portafolio/browser/index.csr.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
